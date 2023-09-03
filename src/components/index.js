841bb6e11794da9b6d8f49480f9f81026a95681c
Zaib Ali <zaibali615@gmail.com>
2023-09-03 21:34:43 +0530

    Exporting components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Article} from './Articles/Articles';
+export {default as Brand} from './brand/Brand';
+export {default as CTA} from './cta/Cta';
+export {default as Features} from './features/Features';
+export {default as Navbar} from './Navbar/Navbar';
