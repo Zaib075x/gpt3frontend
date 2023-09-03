4b29124c9f7cfd2b265c698b60edc28828d0022c
Zaib Ali <zaibali615@gmail.com>
2023-09-03 22:24:47 +0530

    Assets add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { RiMenuLine,RiCloseLine} from 'react-icons/ri';
 import './navbar.css'
 
+//BEM -> Block Element Modifier {Naming Convention for CSS}
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links-logo">
+          
+        </div>
+      </div>
+
+    </div>
   )
 }
 
